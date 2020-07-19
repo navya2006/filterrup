4cd9c0f1652bd51d40dfb84ddba0e88eebc3db38
navya2006 <navya2006.github.com>
2020-07-19 09:41:08 +0530

    Loader added

---
+++ index.js
@@ -0,0 +1,11 @@
+    // LOADER
+    
+   var myVar;
+   function myFunction() {
+      myVar = setTimeout(showPage, 4000);
+   }
+    
+   function showPage() {
+      document.getElementById("load").style.display = "none";
+      document.getElementById("mainBod").style.display = "block";
+   } 
